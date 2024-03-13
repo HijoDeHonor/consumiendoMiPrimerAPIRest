713a68fbb7d56ad08460410e4b6bc3527fe22abf
Nicolas Grandoli <41nicograndoli@gmail.com>
2024-03-13 16:39:49 -0300

    mejoras en el codigo

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ fetch(URL, {
     mode: 'cors',
     headers: {
         'Content-Type': 'application/json',
-        
+
     }
 })
     .then(responce => responce.json())   //con el resolve de esta promesa consigo el arreglo con los objetos dentro del json
     .then(data => {
 
-     console.log(data)
-        const moviesArr= data;
-        moviesArr.map((element) => {
+
+        const moviesArr = data;
+        moviesArr.foreach((element) => {
             const title = element.title
             const image = element.imgURL
-            const dir   = element.director
-            
-            const poster = 
-            `
+            const director = element.director
+
+            const poster =
+                `
                 <div>
                     <img src="${image}">
                     <h2>${title}</h2>
-                    <small>${dir}</small>
+                    <small>${director}</small>
                 </div>
             `
-            document.getElementById('container').innerHTML+= poster
+            document.getElementById('container').innerHTML += poster
         })
     })
-        
-.catch(err => {
-    console.log(err)
-});
+
+    .catch(err => {
+        console.log(err)
+    });
